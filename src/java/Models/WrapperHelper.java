224ee38095e16daf2b16a86b4a5c1759bec1ed74
ksheppard <kyranshepp89@hotmail.com>
2016-02-02 11:18:59 +0000

    adding features to extract text from the html

--- src/java/Models/WrapperHelper.java
+++ src/java/Models/WrapperHelper.java
@@ -27,6 +27,9 @@ import org.jsoup.select.Elements;
  */
 public class WrapperHelper {
 
+    String html;
+    private final int NUM_OF_CHARS = 150;
+    
     public WrapperHelper() {
     }
 
@@ -34,32 +37,53 @@ public class WrapperHelper {
         List<SiteFeatures> siteList = getDataFromFile(inputStream);
         List<Wrapper> wrapperList = new ArrayList<Wrapper>();
         for (int i = 0; i < siteList.size(); i++) {
-            wrapperList.add(generateWrapper(siteList.get(i)));
+            //wrapperList.add(generateWrapper(siteList.get(i)));
+            wrapperList.add(generateFromText(siteList.get(i)));
         }
-        Wrapper wrapper = aggregateWrappers(wrapperList);
+       // Wrapper wrapper = aggregateWrappers(wrapperList);
         //save into db at end
     }
 
-    private Wrapper generateWrapper(SiteFeatures feature) {
-        Wrapper wrapper = null;
+//    private Wrapper generateWrapper(SiteFeatures feature) {
+//        Wrapper wrapper = null;
+//        List<Rule> ruleList = new ArrayList<Rule>();
+//        try {
+//            Document doc = Jsoup.connect(feature.getUrl()).get();
+//            Elements elements = doc.getAllElements();
+//            Iterator it = feature.getFeatureMap().entrySet().iterator();
+//            while (it.hasNext()) {
+//                Map.Entry pair = (Map.Entry) it.next();
+//                FeatureEnum featureEnum = FeatureEnum.valueOf(pair.getKey().toString());
+//                String value = pair.getValue().toString();
+//                it.remove(); // avoids a ConcurrentModificationException
+//                if (pair.getValue().toString() != "") {
+//                    Elements searchElements = elements.select(String.format("body *:containsOwn(%s)", value));
+//                    if (searchElements.size() > 0) {
+//                        searchElements = filterSearchElements(searchElements, value);
+//                    }
+//
+//                    for (int i = 0; i < searchElements.size(); i++) {
+//                        ruleList.add(createRule(featureEnum, value, searchElements.get(i)));
+//                    }
+//                }
+//            }
+//
+//        } catch (IOException ex) {
+//            Logger.getLogger(WrapperHelper.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        return wrapper;
+//    }
 
+    private Wrapper generateFromText(SiteFeatures feature) {
+        //make so isnt caps sensitive? aka convert everything to lower
+        Wrapper wrapper = null;
+        html = null;
         try {
             Document doc = Jsoup.connect(feature.getUrl()).get();
-            Elements elements = doc.getAllElements();
-            Iterator it = feature.getFeatureMap().entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry) it.next();
-                System.out.println(pair.getKey() + " = " + pair.getValue());
-                it.remove(); // avoids a ConcurrentModificationException
-                if (pair.getValue().toString() != "") {
-                    Elements searchElements = elements.select(String.format("*:contains(>%s<)", pair.getValue().toString()));
-                    for (int i = 0; i < searchElements.size(); i++) {
-                        //want to put all elements in to the next stage if possible as aggregation stage can weed out the ones that wont work
-                        Element element = searchElements.get(i);
-                    }
-                    System.out.println("");
-                }
-            }
+            html = doc.body().toString();
+            int length = html.length();
+            wrapper = createWrapperFromSearch(getDataFromFile(feature));
 
         } catch (IOException ex) {
             Logger.getLogger(WrapperHelper.class.getName()).log(Level.SEVERE, null, ex);
@@ -68,9 +92,100 @@ public class WrapperHelper {
         return wrapper;
     }
 
-    private Wrapper aggregateWrappers(List<Wrapper> wrapperList) {
-        return null;
+    private List<WrapperSearchResult> getDataFromFile(SiteFeatures feature) {
+        List<WrapperSearchResult> searchList = new ArrayList<WrapperSearchResult>();
+        Iterator it = feature.getFeatureMap().entrySet().iterator();
+        
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            FeatureEnum featureEnum = FeatureEnum.valueOf(pair.getKey().toString());
+            String value = pair.getValue().toString();
+            if(value == "") continue;
+            it.remove(); // avoids a ConcurrentModificationException
+            int index = 0;
+            while (true) {
+                index = html.indexOf(value, index);
+                if (index == -1) {
+                    break;
+                } else {
+                    searchList.add(new WrapperSearchResult(index, value, featureEnum));
+                }
+                index += value.length();
+            }
+            System.out.println("");
+        }
+        return searchList;
     }
+    
+    private Wrapper createWrapperFromSearch(List<WrapperSearchResult> searchList){
+        Wrapper wrapper = null;
+        
+        for (int i = 0; i < searchList.size(); i++) {
+            WrapperSearchResult searchResult = searchList.get(i);
+            Rule rule = new Rule(searchResult.getFeature());
+            rule.setLeft(setLeft(searchResult, searchList, i));
+            rule.setRight(setLeft(searchResult, searchList, i));
+            
+        }
+        
+        return wrapper;
+    }
+    
+    private String setLeft(WrapperSearchResult searchResult, List<WrapperSearchResult> searchList, int index){
+        //index and rest of list is so can stop when reaches other data?
+        //could put this in the setter for left and right within the rule class - do later
+        String left = html.substring(searchResult.getStartIndex() - 1 - NUM_OF_CHARS, searchResult.getStartIndex() - 1);
+        
+        return left;
+    }
+    
+    private String setRight(WrapperSearchResult searchResult, List<WrapperSearchResult> searchList, int index){
+        
+        String right = html.substring(searchResult.getStartIndex() + searchResult.getValue().length(), searchResult.getStartIndex() + searchResult.getValue().length() + NUM_OF_CHARS);
+        
+        return right;
+    }
+
+    private Rule createRule(FeatureEnum feature, String value, Element element) {
+
+        Rule rule = new Rule(feature);
+        int index = element.toString().indexOf(value);
+        rule.setLeft(element.toString().substring(0, index - 1));
+        rule.setLeft(element.toString().substring(index + value.length()));
+        //ignore open and close for now
+
+        return rule;
+    }
+
+//    private Elements filterSearchElements(Elements searchElements, String val) {
+////        Elements filteredElements = new Elements();
+////        for (int i = 0; i < searchElements.size(); i++) {
+////            //want to put all elements in to the next stage if possible as aggregation stage can weed out the ones that wont work
+////            Element element = searchElements.get(i);
+////            String test = element.text();
+////            String id = element.id();
+////            if(element.text().contains(String.format(">%s<", val))){
+////                System.out.println("");
+////            }
+////            
+////        }
+////        filteredElements.add(null);
+//
+////        return filteredElements;
+//        return searchElements;
+//    }
+//
+//    private Wrapper aggregateWrappers(List<Wrapper> wrapperList) {
+//        //put collection of wrapper with mess of rules in here and then will go from there
+//        return null;
+//    }
+//
+//    private List<Rule> aggregateRuleList(List<Rule> ruleList) {
+//        // can have multiple potential rules for same feature in same domain going in here
+//        //this will try and get the rule that matches all and tests, if doesnt work could potentially move to next set of rules for that feature 
+//        //(this is where web page contains same bit of info in different places)
+//        return null;
+//    }
 
     public void testSystem() {
 
---
+++ src/java/Models/WrapperSearchResult.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Kyran
+ */
+public class WrapperSearchResult {
+    //used to store the data that find in html, holds the position of each
+    
+    private int startIndex;
+    private String value;
+    private FeatureEnum feature;
+
+    public WrapperSearchResult(int startIndex, String value, FeatureEnum feature) {
+        this.startIndex = startIndex;
+        this.value = value;
+        this.feature = feature;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public FeatureEnum getFeature() {
+        return feature;
+    }
+
+    public void setFeature(FeatureEnum feature) {
+        this.feature = feature;
+    }
+    
+    
+}
